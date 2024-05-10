c9ccef5a65f58220e3533cb90adc553e91e8d1a7
daniel <tishakovdanyilgmail.com>
2024-05-10 14:31:11 +0100

    tables compleate

--- models.py
+++ models.py
@@ -1,4 +1,51 @@
-from sqlalchemy import Column, Integer, String, Table, ForeignKey, Boolean, func
+from sqlalchemy import Column, Integer, Float, String, Table, ForeignKey, Boolean, func
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy.sql.sqltypes import DateTime
+
+
+Base = declarative_base()
+
+class Group(Base):
+    __tablename__ = "groups"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(25), nullable=False)
+
+
+class Student(Base):
+    __tablename__ = "students"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(35), nullable=False)
+    goup_id = Column(Integer, ForeignKey("groups.id"))
+    group = relationship("Group", back_populates="students")
+
+Group.students = relationship("Student", back_populates="group")
+
+
+class Teacher(Base):
+    __tablename__ = "teachers"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(35), nullable=False)
+    subjects = relationship("Subject", back_populates="teacher")
+
+
+class Subject(Base):
+    __tablename__ = "subjects"
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    teacher_id = Column(Integer, ForeignKey("teachers.id"), nullable=False)
+    teacher = relationship("Teacher", back_populates="subjects")
+    
+class Grade(Base):
+    __tablename__ = "grades"
+    id = Column(Integer, primary_key=True)
+    student_id = Column(Integer, ForeignKey("students.id"), nullable=False)
+    subject_id = Column(Integer, ForeignKey("subjects.id"), nullable=False)
+    grade = Column(Float, nullable=False)
+    date_received = Column(DateTime(timezone=True), default = func.now()) #onupdate - change date if update default - create date
+    student = relationship("Student", back_populates="grades")
+    subject = relationship("Subject", back_populates="grades")
+
+Student.gardes = relationship("Grade", back_populates="student")
+Subject.gardes = relationship("Grade", back_populates="subject")
+
